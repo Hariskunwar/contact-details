683aba73b27f9df740c19463b5146aac01e4ef46
Harish Kunwar <kunwarharish11@gmail.com>
2024-05-14 21:01:28 +0545

    Project initialized

---
+++ server.js
@@ -0,0 +1,11 @@
+const express=require("express");
+require("dotenv").config();
+
+
+const app=express();
+
+
+const PORT=process.env.PORT||4000;
+app.listen(PORT,()=>{
+    console.log(`Server running at port : ${PORT}`);
+});
